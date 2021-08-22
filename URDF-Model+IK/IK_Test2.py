bcf42456ccfea4b51fc73fb74fd7e688d4847e27
BR <simbingrui@gmail.com>
2021-08-23 00:00:38 +0800

    Adding 7DOF URDF+IK

--- URDF-Model+IK/IK_Test2.py
+++ URDF-Model+IK/IK_Test2.py
@@ -1,24 +1,19 @@
 import pybullet as p
 import time
-import pybullet_data
-import os
+
+coordinate=[.15,.18,.14]
+
+file_name = "5DOFArm.urdf"
 p.connect(p.GUI)
-coordinate=[2,2,1.5]
+arm = p.loadURDF(file_name, useFixedBase=1)
 
-StartPos = [1,1,1]
-StartOrientation = p.getQuaternionFromEuler([0,0,0])
-arm = p.loadURDF("Arm2.urdf", useFixedBase=1)
-
-p.loadURDF(os.path.join(pybullet_data.getDataPath(), "plane.urdf"), 0, 0, 0)
 
 numJoints = p.getNumJoints(arm)
 for i in range(numJoints):
     print(p.getJointInfo(arm,i))
     
-p.setGravity(0, 0, 0)
 
-
-angle1, angle2, angle3, angle4=p.calculateInverseKinematics(arm,7,coordinate,residualThreshold=0.02)
+angle1, angle2, angle3, angle4, angle5=p.calculateInverseKinematics(arm,6,coordinate,residualThreshold=0.001)
 p.setJointMotorControl2(bodyIndex=arm,
                         jointIndex=0,
                         controlMode=p.POSITION_CONTROL,
@@ -26,26 +21,33 @@ p.setJointMotorControl2(bodyIndex=arm,
                         force=20)
 
 p.setJointMotorControl2(bodyIndex=arm,
-                        jointIndex=2,
+                        jointIndex=1,
                         controlMode=p.POSITION_CONTROL,
                         targetPosition=angle2,
                         force=20)
 
 p.setJointMotorControl2(bodyIndex=arm,
-                        jointIndex=5,
+                        jointIndex=3,
                         controlMode=p.POSITION_CONTROL,
                         targetPosition=angle3,
                         force=20)
 
 
 p.setJointMotorControl2(bodyIndex=arm,
-                        jointIndex=7,
+                        jointIndex=4,
                         controlMode=p.POSITION_CONTROL,
                         targetPosition=angle4,
                         force=20)
 
+p.setJointMotorControl2(bodyIndex=arm,
+                        jointIndex=6,
+                        controlMode=p.POSITION_CONTROL,
+                        targetPosition=angle5,
+                        force=20)
+
+print(angle1, angle2, angle3, angle4, angle5)
 while True:
 
     p.stepSimulation()
-    print(p.getLinkState(arm,7,computeForwardKinematics=1))
-    time.sleep(1. / 240.)
+    print(p.getLinkState(arm,6,computeForwardKinematics=1))
+    time.sleep(0.1)
