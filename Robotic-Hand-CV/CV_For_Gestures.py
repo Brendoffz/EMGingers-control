a19c3ecd1399a5ccb71178beaade7f77b2747f8f
bbrr132 <simbingrui@gmail.com>
2021-10-08 17:27:31 +0800

    adding Puchase

--- Robotic-Hand-CV/CV_For_Gestures.py
+++ Robotic-Hand-CV/CV_For_Gestures.py
@@ -1,13 +1,19 @@
 from cvzone.HandTrackingModule import HandDetector
 import cv2 as cv
 
+
 frame = None
 key = None
 
 fingerDetector = HandDetector(detectionCon=0.7, maxHands=1)
 cap=cv.VideoCapture(1)
+#cap.set(cv.CAP_PROP_FRAME_WIDTH, 2560)
+#cap.set(cv.CAP_PROP_FRAME_HEIGHT, 960)
+
+
 while 1:
     success, frame = cap.read()
+    #frame = cv.flip(frame, 1)
     hands, frame = fingerDetector.findHands(frame)
     if hands:
         hand1 = hands[0]
@@ -15,9 +21,9 @@ while 1:
         boundingBox1 = hand1["bbox"] 
         centerPoint1 = hand1["center"] 
         centerPoint1 = hand1["type"] 
-        fingers = fingerDetector.fingersUp(hand1)
-        print(fingers)
-       
+        #fingers = fingerDetector.fingersUp(hand1)
+        #print(fingers)
+    
     cv.imshow("Hand Video",frame)
     key = cv.waitKey(500)
     if key == (ord('q')):
