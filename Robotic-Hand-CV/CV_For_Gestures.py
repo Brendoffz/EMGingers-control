791513f487c284f963c53457039dbe1cc29da366
bbrr132 <simbingrui@gmail.com>
2021-10-24 16:27:09 +0800

    added control of arm

--- Robotic-Hand-CV/CV_For_Gestures.py
+++ Robotic-Hand-CV/CV_For_Gestures.py
@@ -1,7 +1,12 @@
 from cvzone.HandTrackingModule import HandDetector
 import cv2 as cv
-
-
+import requests 
+import requests as reqs 
+url1="http://192.168.10.103/get?input1="
+url2="http://192.168.10.103/get?input2="
+url3="http://192.168.10.103/get?input3="
+url4="http://192.168.10.103/get?input4="
+url5="http://192.168.10.103/get?input5="
 frame = None
 key = None
 
@@ -23,9 +28,15 @@ while 1:
         centerPoint1 = hand1["type"] 
         fingers = fingerDetector.fingersUp(hand1)
         print(fingers)
+        reqs.get(url1+str(180*fingers[0]))
+        reqs.get(url2+str(180*fingers[1])) 
+        reqs.get(url3+str(180*fingers[2])) 
+        reqs.get(url4+str(180*fingers[3])) 
+        reqs.get(url5+str(180*fingers[4])) 
     
     cv.imshow("Hand Video",frame)
     key = cv.waitKey(500)
+    
     if key == (ord('q')):
         break
     
